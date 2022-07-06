68242090e9f29ec4ae8a905624601853e6141d52
RVIRUS0817 <t.virus.1989@gmail.com>
2022-07-06 14:09:34 +0900

    fix

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 It is a program to export to S3 with lambda from the previous day snapshot of RDS.
 
+https://blog.adachin.me/archives/49717
+
 ![スクリーンショット 2022-07-05 15 08 58](https://user-images.githubusercontent.com/5633085/177260914-72bd06bf-a118-464f-a2dc-679046f0bcbc.jpg)
 
 
@@ -9,13 +11,14 @@ It is a program to export to S3 with lambda from the previous day snapshot of RD
 
 https://docs.aws.amazon.com/ja_jp/AmazonRDS/latest/UserGuide/USER_ExportSnapshot.html
 
-- RDS
-  - snapshot
-- EventBridge
-- S3
-- Glue
-- Athena
-- KMS
 - Lambda
   - Python3.9
+- EventBridge Rule
+- KMS
+- RDS
+  - snapshot
+- S3
+- AWS Glue
+- Amazon Athena
+- Redash
 
--- lambda-handler.py
+++ lambda-handler.py
@@ -1,29 +1,29 @@
+import os
 import json
 import boto3
 from datetime import datetime,date,timedelta
-
-S3_BUCKET_NAME="xxxxxxxxxx"
-IAM_ROLE_ARN="arn:aws:iam::xxxxxxxxxxxx:role/lambda-rds-s3-export"
-KMS_KEY_ID="arn:aws:kms:ap-northeast-1:xxxxxxxxxx:key/xxxxxxxxxxxxxxxxxxxxx"
-
+ 
+S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+IAM_ROLE_ARN = os.environ['IAM_ROLE_ARN']
+KMS_KEY_ARN = os.environ['KMS_KEY_ARN']
+ 
 client = boto3.client('rds')
-
+ 
 def lambda_handler(event, context):
-
+ 
     today = datetime.today()
     yesterday = today - timedelta(days=1)
-
+ 
     SOURCE_ARN="arn:aws:rds:ap-northeast-1:xxxxxxxxxx:cluster-snapshot:rds:xxxx-" + datetime.strftime(yesterday, '%Y-%m-%d-xx-xx')
-    export_task_identifier="xxxxxxxx" + datetime.now().strftime("%Y%m%d%H%M%S")
-
+    EXPORT_TASK_IDENTIFIER="hoge" + datetime.now().strftime("%Y%m%d%H%M%S")
+ 
     response = client.start_export_task(
-        ExportTaskIdentifier=export_task_identifier,
+        ExportTaskIdentifier=EXPORT_TASK_IDENTIFIER,
         SourceArn=SOURCE_ARN,
         S3BucketName=S3_BUCKET_NAME,
         IamRoleArn=IAM_ROLE_ARN,
-        KmsKeyId=KMS_KEY_ID,
-#        ExportOnly=[
-#        'hoge',
-#        ]
+        KmsKeyId=KMS_KEY_ARN,
+        ExportOnly=[
+        'hoge',
+        ]
     )
-
